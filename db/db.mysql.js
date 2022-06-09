953b9e7617a54b51a0ac2ea535eda96bd9ce2710
jpmazel <jpmazel@gmail.com>
2022-06-09 10:20:25 +0200

    ajout des variables d'environnement sur la base de données

--- db/db.mysql.js
+++ db/db.mysql.js
@@ -4,13 +4,16 @@ dotenv.config();
 
 //importer mysql
 const mysql = require("mysql");
+console.log("process.env.reseau_social")
+console.log(process.env.DB_NAME)
 
 //les paramètres de connexion à la base de données
 const mysqlconnection = mysql.createConnection({
   host: 'localhost',
-  database: 'reseau_social',
-  user: 'root',
-  password: ''
+  // database: `${process.env.DB_NAME}`,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password:  process.env.DB_PASSWORD
 })
 
 //la connexion à la base de données
